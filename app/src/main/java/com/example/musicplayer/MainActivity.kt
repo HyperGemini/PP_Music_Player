198d6aa9ebdf240a255f23f04a632844f36079a6
LimunToS <alexalukac@gmail.com>
2021-05-03 20:58:09 +0200

    Play music sound

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.musicplayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.fragment_gui.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewPager: ViewPager2
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var runnable: Runnable
+    private var handler: Handler = Handler()
+    private var pause:Boolean =false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +30,64 @@ class MainActivity : AppCompatActivity() {
         // The pager adapter, which provides the pages to the view pager widget
         val pagerAdapter = FragmentAdapter(this)
         viewPager.adapter = pagerAdapter
+
+
+        btn_playpause.setOnClickListener {
+            if(pause){
+                mediaPlayer.seekTo(mediaPlayer.currentPosition)
+                mediaPlayer.start()
+                pause = false
+                Toast.makeText(this,"media playing",Toast.LENGTH_SHORT).show()
+            }else{
+
+                mediaPlayer = MediaPlayer.create(applicationContext,R.raw.dancewithsomebody)
+                mediaPlayer.start()
+                Toast.makeText(this,"media playing",Toast.LENGTH_SHORT).show()
+                pause = true
+            }
+            initializeSeekBar()
+
+
+        }
+        slider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                if (b) {
+                    mediaPlayer.seekTo(i * 1000)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
+
+
     }
+    private fun initializeSeekBar() {
+        slider.max = mediaPlayer.seconds
+
+        runnable = Runnable {
+            slider.progress = mediaPlayer.currentSeconds
+
+            tv_current_time.text = "${mediaPlayer.currentSeconds} sec"
+            val diff = mediaPlayer.seconds - mediaPlayer.currentSeconds
+            tv_total_time.text = "$diff sec"
+
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.postDelayed(runnable, 1000)
+    }
+
 }
+
+val MediaPlayer.seconds:Int
+    get() {
+        return this.duration / 1000
+    }
+// Creating an extension property to get media player current position in seconds
+val MediaPlayer.currentSeconds:Int
+    get() {
+        return this.currentPosition/1000
+    }
